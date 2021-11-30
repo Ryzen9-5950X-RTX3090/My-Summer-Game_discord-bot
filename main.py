a87db5970af7e4178724b8121f11d24b529ab9be
Ryzen9-5950X-RTX3090 <jordanluders@gmail.com>
2021-11-30 18:54:44 +0000

    Fix the bot.

--- main.py
+++ main.py
@@ -15,114 +15,44 @@ async def on_message(message):
   if message.content.startswith('hello'):
     await message.channel.send('Hello!')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
   if message.content.startswith('hru'):
     await message.channel.send('I am doing great, you?')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
   if message.content.startswith('great'):
     await message.channel.send('That is great to hear!')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
   if message.content.startswith('terrible'):
     await message.channel.send('What a shame!')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
   if message.content.startswith('sup'):
     await message.channel.send('sup weirdo')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
   if message.content.startswith('weirdest'):
     await message.channel.send('no u')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
   if message.content.startswith('ur mom'):
     await message.channel.send('no urs')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
   if message.content.startswith('bye'):
     await message.channel.send('Bye weirdo!')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
   if message.content.startswith('gn'):
     await message.channel.send('gn weirdo!')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
   if message.content.startswith('ttyl'):
     await message.channel.send('Ok see ya then')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
   if message.content.startswith('!release-date'):
     await message.channel.send('Hello! The planned release date for My Summer Game is for Q4 of 2022!')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
   if message.content.startswith('!ro.build.info'):
     await message.channel.send('Closed beta 0.4.4, built on December 1, 2021, released on December 1, 2021.')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
   if message.content.startswith('!download'):
     await message.channel.send('https://github.com/Ryzen9-5950X-RTX3090/My-Summer-Game_Unity-project/releases')
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
   if message.content.startswith('!unity-info'):
     await message.channel.send('Unity 2D personal, version 2021.2.4f1')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
 
   if message.content.startswith('!source-code'):
     await message.channel.send('https://github.com/Ryzen9-5950X-RTX3090/My-Summer-Game_Unity-project/')
